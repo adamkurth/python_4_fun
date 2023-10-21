c802ae709ac2384ac58d47cee5edf9476dc0f529
Adam Kurth <amkurth@asu.edu>
2023-10-21 16:25:51 -0700

    adding duplicates

--- visualize_gradient_descent.R
+++
@@ -1,6 +0,0 @@
-rm(list=ls())
-setwd("/Users/adamkurth/Documents/vscode/Python/linear_regression_live-master/")
-data = read.csv('output_grad.csv')
-weight = data[,1]
-bias = data[,2]
-error = data[,3]
